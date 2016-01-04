afc7b1f2a5600429cee01bb9553b2da44b56a94f
setu <setu.1421@yahoo.com>
2016-01-04 22:15:36 +0600

    changed aspect ratio and rotation

--- app/build/intermediates/res/debug/raw/simple_vertex_shader.glsl
+++ app/build/intermediates/res/debug/raw/simple_vertex_shader.glsl
@@ -1,4 +1,5 @@
-attribute vec4 a_Position;  
+uniform mat4 u_Matrix;
+attribute vec4 a_Position;
 attribute vec4 a_Color;
 
 varying vec4 v_Color;
@@ -7,6 +8,6 @@ void main()
 {                            
     v_Color = a_Color;
 	  
-    gl_Position = a_Position;    
+    gl_Position = u_Matrix * a_Position;
     gl_PointSize = 10.0;          
 }          
--- app/src/main/java/com/airhockey/android/AirHockeyRenderer.java
+++ app/src/main/java/com/airhockey/android/AirHockeyRenderer.java
@@ -19,10 +19,17 @@ import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
 import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUniformMatrix4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
+import static android.opengl.Matrix.orthoM;
+import static android.opengl.Matrix.rotateM;
+import static android.opengl.Matrix.setIdentityM;
+import static android.opengl.Matrix.translateM;
+import static android.opengl.Matrix.multiplyMM;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -37,12 +44,19 @@ import android.opengl.GLSurfaceView.Renderer;
 import android.util.Log;
 
 import com.airhockey.android.util.LoggerConfig;
+import com.airhockey.android.util.MatrixHelper;
 import com.airhockey.android.util.ShaderHelper;
 import com.airhockey.android.util.TextResourceReader;
 
 public class AirHockeyRenderer implements Renderer {    
     private static final String A_POSITION = "a_Position";
-    private static final String A_COLOR = "a_Color";    
+    private static final String A_COLOR = "a_Color";
+
+    private static final String U_MATRIX = "u_Matrix";
+    private final float[] projectionMatrix = new float[16];
+    private final float[] modelMatrix = new float[16];
+    private int uMatrixLocation;
+
     private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int COLOR_COMPONENT_COUNT = 3;    
     private static final int BYTES_PER_FLOAT = 4;
@@ -328,6 +342,7 @@ public class AirHockeyRenderer implements Renderer {
         
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
         aColorLocation = glGetAttribLocation(program, A_COLOR);
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX);
 
         // Bind our data, specified by the variable vertexData, to the vertex
         // attribute at location A_POSITION_LOCATION.
@@ -361,6 +376,29 @@ public class AirHockeyRenderer implements Renderer {
     public void onSurfaceChanged(GL10 glUnused, int width, int height) {
         // Set the OpenGL viewport to fill the entire surface.
         glViewport(0, 0, width, height);
+
+        /*final float aspectRatio = width > height ?
+                (float) width / (float) height :
+                (float) height / (float) width;
+        if (width > height) {
+            // Landscape
+            orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+        } else {
+            // Portrait or square
+            orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+        }*/
+
+        MatrixHelper.perspectiveM(projectionMatrix, 45, (float) width
+                / (float) height, 1f, 10f);
+
+        setIdentityM(modelMatrix, 0);
+
+        translateM(modelMatrix, 0, 0f, 0f, -3.5f);
+        rotateM(modelMatrix, 0, 60f, 0f, 0f, 1f);
+
+        final float[] temp = new float[16];
+        multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
+        System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
     }
 
     /**
@@ -384,6 +422,7 @@ public class AirHockeyRenderer implements Renderer {
         // Draw the second mallet.
         glDrawArrays(GL_POINTS, 9, 1);*/
         glClear(GL_COLOR_BUFFER_BIT);
+        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
 
         //line1
 
